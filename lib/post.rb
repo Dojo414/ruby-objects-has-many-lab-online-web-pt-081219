a15aff5f6e03c2c86f07bb02d923302ca193c5e5
Donovan Jones <donovanljones94@gmail.com>
2019-09-24 14:24:54 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -2,7 +2,7 @@ class Post
   attr_accessor :title, :author 
   @@all = []
   
-  def initialize(name, author=nil)
+  def initialize(title, author=nil)
     @title = title
     @@all << self
     @author = author
